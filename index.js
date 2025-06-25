63594a317b1b6e7608c178fb046108d3f592c498
Labidi karim <karim.labidi@esprit.tn>
2025-06-25 10:53:47 +0000

    maj de index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log("Hello RSI");
-const date = new Date();
-console.log("Date du jour et heure actuelle : ", date.toISOString());
+const http = require("http");
+const hostname = "localhost";
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello RSI , notre premiere app Nodejs");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+// console.log("Hello RSI");
+// const date = new Date();
+// console.log("Date du jour et heure actuelle : ", date.toISOString());
